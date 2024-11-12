8f7d85b21971563adf90f7a909d8348e3c8a3571
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2024-11-11 21:06:03 -0300

    Implementação comunicação bluetooth

--- aplicativo/app/src/main/java/br/com/mateus/brito/appautomacaoestacionamento/MainActivity.java
+++ aplicativo/app/src/main/java/br/com/mateus/brito/appautomacaoestacionamento/MainActivity.java
@@ -1,6 +1,10 @@
 package br.com.mateus.brito.appautomacaoestacionamento;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -10,6 +14,7 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,9 +22,23 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnQrCode;
+    private static final String DEVICE_ADDRESS = "98:D3:21:F8:38:19";
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket bluetoothSocket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    private Handler handler;
+    private byte[] buffer = new byte[1024];
+    private int bytes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        this.btnQrCode = (Button) findViewById(R.id.scanQrButton);
-        this.btnQrCode.setOnClickListener(new View.OnClickListener() {
+        Button btnQrCode = (Button) findViewById(R.id.scanQrButton);
+        btnQrCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
@@ -46,30 +65,93 @@ public class MainActivity extends AppCompatActivity {
                 integrator.initiateScan();
             }
         });
+
+        this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        this.handler = new Handler();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        LoadingAlert loadingAlert = new LoadingAlert(this);
-        loadingAlert.startAlertDialog();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                loadingAlert.closeAlertDialog();
-            }
-        }, 2500);
-
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
-            if (result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-                // Aqui você pode adicionar a lógica para processar o QR-Code escaneado, como a inserção do CPF
-            }
+            LoadingAlert loadingAlert = new LoadingAlert(this);
+            loadingAlert.startAlertDialog();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    loadingAlert.closeAlertDialog();
+
+                    if (result.getContents() == null) {
+                        Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                        connectToDevice(result.getContents());
+                    }
+                }
+            }, 2500);
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            bluetoothSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void connectToDevice(String address) {
+        BluetoothDevice device = this.bluetoothAdapter.getRemoteDevice(address);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "No permission", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            this.bluetoothSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            this.bluetoothSocket.connect();
+            this.outputStream = bluetoothSocket.getOutputStream();
+            this.inputStream = bluetoothSocket.getInputStream();
+            sendData('A');
+            listenForData();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            Toast.makeText(this, "Bluetooth connection failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void sendData(char data) {
+        try {
+            outputStream.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listenForData() {
+        new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    try {
+                        bytes = inputStream.read(buffer);
+                        handler.post(new Runnable() {
+                            public void run() {
+                                char data = (char) buffer[0];
+                                Toast.makeText(MainActivity.this, "Arduino: " + data, Toast.LENGTH_SHORT).show();
+                                //USAR O 'data' para decidir o que fazer com o que receber do arduino
+                                //exibir se tem vagas
+                                //desligar a conexão com o bluetooth
+                            }
+                        });
+                    } catch (IOException e) {
+                        break;
+                    }
+                }
+            }
+        }).start();
     }
 }
